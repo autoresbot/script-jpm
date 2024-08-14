13a21a834a2d2507de9e0797f4e4b1b01fa661a1
autoresbot <autoresbot@gmail.com>
2024-08-14 22:13:27 +0700

    version 1.0 created

---
+++ README.md
@@ -0,0 +1,6 @@
+// =============================================
+// VERSION UPDATE
+// UPDATE TERAKHIR 14 Juli 2024 JAM 00:00 WIB
+// WEBSITE : autoresbot.com
+// VERSION : JPM 1.0.0
+// =============================================
---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+const numberAllowed = ['6285246154386','62xxxxxxxxx']; // Nomor yang diizinkan untuk chat ke bot, tambahkan kalau diperlukan
+
+global.prefix = ['.', '#']; // Daftar prefix
+
+global.jeda = 3000; // 3 detik jeda pengiriman untuk pushkontak atau broadcast
+
+global.name_script = 'Script Jpm';
+
+global.version = '1.0';
+
+
+module.exports = { numberAllowed};
